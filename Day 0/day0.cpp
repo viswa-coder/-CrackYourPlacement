e4ec4fc2eb21d4afc769793fd54611bf1f572925
viswa-coder <viswatejdeep@gmail.com>
2024-07-16 22:43:34 +0530

    Initial commit for #CrackYourPlacement repository

---
+++ Day 0/day0.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout << "This is Day 0" <<endl;
+  return 1;
+}
---
+++ Day 0/dummy.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout << "This is Dummy" <<endl;
+  return 1;
+}
